773fdc87e811dfa129a1763b6c646912434de59e
crscillitoe <christian.scillitoe@gmail.com>
2020-03-18 22:19:29 -0700

    build

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
+import { HttpClient } from "@angular/common/http";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.scss"]
 })
 export class AppComponent {
-  title = 'WPICTF2020';
+  title = "WPICTF2020";
+
+  message: string = "";
+
+  constructor(httpClient: HttpClient) {
+    httpClient
+      .get("https://wpictf2020.firebaseio.com/message.json")
+      .subscribe((res: string) => {
+        this.message = res;
+      });
+  }
 }
